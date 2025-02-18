9631d444297eda18047a859ac46ce149a1c3d3ac
dudegod1 <vedvarunc@gmail.com>
2025-02-17 21:15:57 -0500

    moredone

--- llproject/src/CircularList.h
+++ llproject/src/CircularList.h
@@ -8,3 +8,24 @@ template <class T>
 struct Node
 {
     T cargo; // Stores the value of the node
+    Node<T>* next; // Pointer to the next node in the circular list
+
+    // Constructor to initialize a node with given cargo and next pointer
+    Node(T cargo, Node<T>* next)
+    {
+        this->cargo = cargo;
+        this->next = next;
+    }
+    #include <type_traits> // Needed for std::is_same
+
+    // Converts the cargo into a string representation
+    string stringy() const
+    {
+        if constexpr (std::is_same<T, int>::value) { // Checks if T is an int
+            return std::to_string(cargo);
+        } else {
+            return string(cargo); // Assumes T is std::string or another string-convertible type
+        }
+    }
+};
+
