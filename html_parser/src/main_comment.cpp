3c115279c388c8c2fb3b4bc94713247e17d9e69e
dudegod1 <vedvarunc@gmail.com>
2025-03-05 11:16:40 -0500

    moremorecode/comments

--- html_parser/src/main_comment.cpp
+++ html_parser/src/main_comment.cpp
@@ -17,20 +17,29 @@ int main() {
     // Variables to hold the indices of the opening and closing tags
     int opening_index = 0;
     int closing_index = 0;
+   
+    // Loop through the HTMl string character by character
     for (int i = 0; i < html_str.length(); i++) {
         if (html_str[i] == '<') {
+            // Found the start of a tag, mark the opening index
             opening_index = i;
         } else if (html_str[i] == '>') {
+            // Found the end of a tag, mark the closing index
             closing_index = i;
+            
+            // Calculate the length of a tag
             int slice_len = closing_index - opening_index;
             string slice = "";
 
             if (html_str[opening_index + 1] == '/') {
+                //If it is a closing tag (it starts with '/'), slice the tag name
                 slice = html_str.substr(opening_index + 2, slice_len - 2);
+                // Check if current closing tag matches top element in the stack
                 if (slice != st.top()) {
                     cout << st.top() << " has no closing tag" << endl;
-                    return 1;
+                    return 1; // Exit if tags don't match
                 }
+                // Pop the matching opening tag from the stack
                 st.pop();
                 continue;
             }
